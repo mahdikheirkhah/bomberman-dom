6bec0581975cf542adbf58ef083eba7a8a9c78c3
mahdikheirkhah <mohammad.kheirkhah@gritlab.ax>
2025-07-01 06:23:17 +0300

    crate a player logic in backend

--- backend/bomberman/Game.go
+++ backend/bomberman/Game.go
@@ -1,10 +1,43 @@
 package bomberman
 
-type GameBorad struct {
+const NumberOfRows = 20
+const NumberOfColumns = 20
+const MaxNumberOfPlayers = 4
+const MinNumberOfPlayers = 2
+
+var Colors = []string{"G", "Y", "R", "B"}
+
+type GameBoard struct {
+	NumberOfPlayers int `json:"numberOfPlayers"`
 }
 
 type GameCell struct {
 	IsOccupied  bool `json:"isOccupied"`
 	IsWall      bool `json:"isWall"`
 	IsExploaded bool `json:"isExploaded"`
+}
+
+func (g *GameBoard) CanCreateNewPlayer() bool {
+	if 0 < g.NumberOfPlayers+1 && g.NumberOfPlayers+1 <= MaxNumberOfPlayers {
+		return true
+	}
+	return false
+}
+
+func (g *GameBoard) FindColor() string {
+	return Colors[g.NumberOfPlayers+1]
+}
+
+func (g *GameBoard) FindStartRowLocation() int {
+	if g.NumberOfPlayers+1 == 1 || g.NumberOfPlayers+1 == 2 {
+		return 0
+	}
+	return NumberOfRows - 1
+}
+
+func (g *GameBoard) FindStartColLocation() int {
+	if g.NumberOfPlayers+1 == 1 || g.NumberOfPlayers+1 == 3 {
+		return 0
+	}
+	return NumberOfColumns - 1
 }
--- backend/bomberman/player.go
+++ backend/bomberman/player.go
@@ -1,10 +1,28 @@
 package bomberman
 
+import "errors"
+
 type Player struct {
-	Name      string `json:"name"`
-	Lives     int    `json:"lives"`
-	Score     int    `json:"score"`
-	Color     string `json:"color"`
-	XLocation int    `json:"xLocation"`
-	YLocation int    `json:"yLocation"`
+	Name   string `json:"name"`
+	Lives  int    `json:"lives"`
+	Score  int    `json:"score"`
+	Color  string `json:"color"`
+	Row    int    `json:"row"`
+	Column int    `json:"column"`
+}
+
+func CreatePlayer(name string, gameboard *GameBoard) (Player, error) {
+	var player Player
+	if !gameboard.CanCreateNewPlayer() {
+		return Player{}, errors.New("Max number of players of has been reached!")
+	}
+
+	player.Name = name
+	player.Lives = 3
+	player.Score = 0
+	player.Color = gameboard.FindColor()
+	player.Row = gameboard.FindStartRowLocation()
+	player.Column = gameboard.FindStartColLocation()
+
+	return player, nil
 }
