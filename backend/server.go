f7d202766ae3f4872ad6b898ec00322f98d4205b
mahdikheirkhah <mohammad.kheirkhah@gritlab.ax>
2025-07-03 19:05:17 +0300

    bind url and the handler

--- backend/server.go
+++ backend/server.go
@@ -3,9 +3,20 @@ package main
 import (
 	"backend/bomberman"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	game := bomberman.InitGame()
 	fmt.Println(game.Panel)
+	// Bind the /ws route
+	http.HandleFunc("/ws", game.HandleWSConnections)
+
+	// Start the server
+	log.Println("Server started at :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
