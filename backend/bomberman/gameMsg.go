acce942e1be051224b452496252ec1b395eef9c5
Oleg Balandin <olegamobile@gmail.com>
2025-07-16 12:02:15 +0300

    remved unnescessary check

--- backend/bomberman/gameMsg.go
+++ backend/bomberman/gameMsg.go
@@ -62,7 +62,7 @@ func (g *GameBoard) HandlePlayerMessages(playerIndex int, conn *websocket.Conn) 
 	log.Printf("Player %d disconnected\n", playerIndex)
     delete(g.PlayersConnections, playerIndex)
     g.Mu.Lock()
-    if playerIndex < len(g.Players) && g.Players[playerIndex].Lives > 0 {
+    if playerIndex < len(g.Players) {
         log.Printf("Player %d lives before disconnect: %d\n", playerIndex, g.Players[playerIndex].Lives)
         g.PlayerDeath(playerIndex)
     }
